5bdee327e0c7a22878f515ac7701821816c59ed4
Omar Diab <odiab@stanford.edu>
2013-02-22 01:12:47 -0800

    made base for page style

--- www/index.php
+++ www/index.php
@@ -1,23 +1,30 @@
-<?php
-  include('Assets.php');
-  Assets::load(TEMPLATE, 'head');
-  $path = $_SERVER['REQUEST_URI'];
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <?php
+    include('Assets.php');
+    Assets::load(TEMPLATE, 'head');
+    $path = $_SERVER['REQUEST_URI'];
+  ?>
 
-<body>
-  <div id="bodyContainer">
-    <?php Assets::load(TEMPLATE, 'header', array('path' => $path)); ?>
-    <div id="bodybox">
+  <body>
+    <div id="topContainer">
+      <div id="sidebar">
+        <span id="sidebarName">IAB</span>
+      </div>
+
       <div id="content">
         <?php
         if (Assets::load(PAGE, $path) != 0) {
           Assets::load(PAGE, '404');
         }
         ?>
+        <div id="bottomPush"></div>
       </div>
-      <?php Assets::load(TEMPLATE, 'footer'); ?>
     </div>
-  </div>
 
-</body>
+    <div id="bottomContainer">
+      <?php Assets::load(TEMPLATE, 'titlebar', array('path' => $path)); ?>
+    </div>
+
+  </body>
 </html>
---
+++ www/templates/titlebar.php
@@ -0,0 +1,11 @@
+<div id="titlebar">
+  <div id="titlebarHeader">
+    <img id="titlebarLogo" src="/assets/images/od-logo.png" alt="O"/>
+    <span id="titlebarName">MAR</span>
+  </div>
+
+  <div id=navigation>
+    <?php //Assets::load(TEMPLATE, 'navigation',
+        //array('path' => $args['path'])); ?>
+  </div>
+</div>
