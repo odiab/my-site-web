769be6a5d5eb7dfcf041fce83d435d219621b4bb
Omar Diab <o.s.diab@gmail.com>
2013-02-23 17:52:37 -0800

    changed homepage placeholder text

--- www/pages/home.php
+++ www/pages/home.php
@@ -1,2 +1,5 @@
 <h1>Hi, I'm Omar.</h1>
 <p>You seem to have stumbled upon my page. Take a look around!</p>
+<h2>But, actually, don't.</h2>
+<p>This site is under construction, and there really isn't that much here.
+But you're welcome to look at my 404 page!</p>
