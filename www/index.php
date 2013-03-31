8f1cc9842f6a0680d4c4281d0ed3414e85468321
Omar Diab <o.s.diab@gmail.com>
2013-03-31 15:26:17 -0700

    moved body tags into body declaration

--- www/index.php
+++ www/index.php
@@ -1,14 +1,11 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/bootstrap/autoload.php');
+$path = $_SERVER['REQUEST_URI'];
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <?php
-  include($_SERVER['DOCUMENT_ROOT'] . '/bootstrap/autoload.php');
-  $path = $_SERVER['REQUEST_URI'];
   Template::load('head');
+  Template::load('body', array('path'=>$path));
   ?>
-
-  <body>
-    <?php
-    Template::load('body', array('path'=>$path));
-    ?>
-  </body>
 </html>
--- www/template/body.php
+++ www/template/body.php
@@ -1,19 +1,21 @@
-<div id="topContainer">
-  <div id="sidebar">
-    <span id="sidebarName">IAB</span>
+<body>
+  <div id="topContainer">
+    <div id="sidebar">
+      <span id="sidebarName">IAB</span>
+    </div>
+
+    <div id="content">
+      <?php
+      if (Page::load($args['path']) != 0) {
+        Page::load('404');
+      }
+      ?>
+      <div id="bottomPush"></div>
+    </div>
   </div>
 
-  <div id="content">
-    <?php
-    if (Page::load($args['path']) != 0) {
-      Page::load('404');
-    }
-    ?>
-    <div id="bottomPush"></div>
+  <div id="bottomContainer">
+    <?php Template::load('titlebar', array('path' => $args['path'])); ?>
+    <?php Template::load('footer'); ?>
   </div>
-</div>
-
-<div id="bottomContainer">
-  <?php Template::load('titlebar', array('path' => $args['path'])); ?>
-  <?php Template::load('footer'); ?>
-</div>
+</body>
