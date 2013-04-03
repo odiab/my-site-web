317b65b2b5885a5caf4d6134a63edfbedf37fec9
Omar Diab <odiab@stanford.edu>
2013-04-03 00:40:24 -0700

    added credits and edu pages, moved asset files, added model autoload

---
+++ www/page/credits.php
@@ -0,0 +1,20 @@
+<h1>Credits</h1>
+<h2>Icons</h2>
+<ul>
+  <li>
+    <a href="http://thenounproject.com/noun/diploma/#icon-No3630"
+      target="_blank">Diploma</a> designed by
+      <a href="http://thenounproject.com/AVEart" target="_blank">
+      Ashley Etheridge</a> from The Noun Project
+  </li>
+  <li>
+    <a href="http://thenounproject.com/noun/telephone/#icon-No5041"
+      target="_blank">Telephone</a> designed by
+      <a href="http://thenounproject.com/DmitryBaranovskiy" target="_blank">
+      Dmitry Baranovskiy</a> from The Noun Project
+  </li>
+  </li>Facebook, LinkedIn, and Github logos property of those companies</li>
+</ul>
+
+<h2>Images</h2>
+<p>Photos were self-taken.</p>
