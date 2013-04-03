201f29a582af3705f65f6223826e3308ebb8a3c0
Omar Diab <odiab@stanford.edu>
2013-04-03 00:39:23 -0700

    gridded layout-ish

--- www/template/titlebar.php
+++ www/template/titlebar.php
@@ -1,10 +1,10 @@
-<div id="titlebar">
-  <div id="titlebarHeader" onclick="window.location = '/'">
-    <img id="titlebarLogo" src="/static/images/od-logo.png" alt="O"/>
-    <span id="titlebarName">MAR</span>
-  </div>
+  <div id="titlebar">
+    <div id="titlebarHeader" onclick="window.location = '/'">
+      <img id="titlebarLogo" src="/static/images/od-logo.png" alt="O"/>
+      <span id="titlebarName">MAR</span>
+    </div>
 
-  <div id='navigation'>
-    <?php Template::load('navigation', array('path' => $args['path'])); ?>
+    <div id='navigation'>
+      <?php Template::load('navigation', array('path' => $args['path'])); ?>
+    </div>
   </div>
-</div>
