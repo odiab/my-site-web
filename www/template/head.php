d3ce50c059908d983dc0aeb59b5ba0721840cbd7
Omar Diab <o.s.diab@gmail.com>
2013-06-18 01:12:18 -0700

    removed unnecessary style appender

--- www/template/head.php
+++ www/template/head.php
@@ -2,11 +2,4 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Omar Diab</title>
   <link rel="stylesheet" href="/static/style/style.php" media="screen" />
-
-  <?php if (isset($styles)) {
-    foreach ($styles as $style) { ?>
-      <link rel="stylesheet" href="/static/style/<?php echo $style ?>"
-        media="screen" />
-    <?php }
-  } ?>
 </head>
