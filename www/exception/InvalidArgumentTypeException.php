73791d7576478c3e452523679f81d43bc67d1b73
Omar Diab <o.s.diab@gmail.com>
2013-06-19 20:16:15 -0700

    added some exception types

---
+++ www/exception/InvalidArgumentTypeException.php
@@ -0,0 +1,10 @@
+<?php
+class InvalidArgumentTypeException extends InvalidArgumentException
+{
+  public function __construct($varname, $intended, $actual)
+  {
+    parent::__construct(
+      $varname . ' must be a ' . $intended . ', instead was a ' . $actual
+    );
+  }
+}
