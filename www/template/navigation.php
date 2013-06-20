6cd45e829219df05fe87dd1137e08a55a6fe371f
Omar Diab <o.s.diab@gmail.com>
2013-06-19 22:07:35 -0700

    reorganized layout and style to be simpler in code

--- www/template/navigation.php
+++ www/template/navigation.php
@@ -25,24 +25,43 @@
   }
 ?>
 
-<ul id="nav">
-  <?php
-  $i = 0;
-  $num = count($links);
-  // generate links to pages
-  foreach ($links as $key => $value) {
-    $class = '';
-    if ($highlight) {
-      if ($current == $key) {
-        $class .= 'current';
+<div id="navLinks">
+  <ul id="navList">
+    <?php
+    $i = 0;
+    $num = count($links);
+    // generate links to pages
+    foreach ($links as $key => $value) {
+      $class = '';
+      if ($highlight) {
+        if ($current == $key) {
+          $class .= 'current';
+        }
       }
-    }
-    if ($i == $num - 1) {
-      $class .= ' last';
-    }
+      if ($i == $num - 1) {
+        $class .= ' last';
+      }
 
-    echo "<li><a class='$class' href='/$key'>$value</a></li>";
-    ++$i;
+      echo "<li><a class='$class' href='/$key'>$value</a></li>";
+      ++$i;
+    }
+    ?>
+  </ul>
+</div>
+
+<div id='navButtons'>
+  <?php
+  $buttons = array(
+    'fb' => 'https://www.facebook.com/omar.s.diab',
+    'linkedin' => 'https://www.linkedin.com/in/osdiab',
+    'github' => 'https://github.com/odiab',
+    'email' => 'mailto:o.s.diab@gmail.com',
+  );
+
+  foreach ($buttons as $name => $link)
+  {
+    echo "<a id='$name-link' href='$link'><img id='$name-img'
+      src='/static/images/buttons/$name-button.png' /></a>";
   }
   ?>
-</ul>
+</div>
--- www/template/titlebar.php
+++ www/template/titlebar.php
@@ -1,26 +1,10 @@
-  <div id="titlebar">
-    <div id="titlebarHeader" onclick="window.location = '/'">
-      <img id="titlebarLogo" src="/static/images/od-logo.png" alt="O"/>
-      <span id="titlebarName">MAR</span>
-    </div>
-
-    <div id='navigation'>
-      <?php Template::load('navigation', array('path' => $args['path'])); ?>
-      <div id='navButtons'>
-        <?php
-        $buttons = array(
-          'fb' => 'https://www.facebook.com/omar.s.diab',
-          'linkedin' => 'https://www.linkedin.com/in/osdiab',
-          'github' => 'https://github.com/odiab',
-          'email' => 'mailto:o.s.diab@gmail.com',
-        );
-
-        foreach ($buttons as $name => $link)
-        {
-          echo "<a id='$name-link' href='$link'><img id='$name-img'
-            src='/static/images/buttons/$name-button.png' /></a>";
-        }
-        ?>
-      </div>
-    </div>
+<div id="titlebar">
+  <div id="titlebarHeader" onclick="window.location = '/'">
+    <img id="titlebarLogo" src="/static/images/od-logo.png" alt="O"/>
+    <span id="titlebarName">MAR</span>
   </div>
+
+  <div id='navigation'>
+    <?php Template::load('navigation', array('path' => $args['path'])); ?>
+  </div>
+</div>
