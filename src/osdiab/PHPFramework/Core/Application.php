6d1ec7d00efbbc087eeb7c2891697bf3324675ed
Omar Diab <odiab@stanford.edu>
2013-07-19 00:53:28 -0700

    Added Application file

---
+++ src/osdiab/PHPFramework/Core/Application.php
@@ -0,0 +1,12 @@
+<?php
+namespace osdiab\PHPFramework\Core;
+
+/**
+ * Represents application-wide assets
+ */
+class Application {
+  public static function getDatabase()
+  {
+    //TODO: implement
+  }
+}
