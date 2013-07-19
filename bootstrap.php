c07dc322e46ef62d2fd9c28af4e158166f2bc4c9
Omar Diab <o.s.diab@gmail.com>
2013-07-19 00:08:57 -0700

    moved vendor/autoload to bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,3 @@
 <?php
 define('SITE_ROOT', __DIR__);
+require_once('vendor/autoload.php');
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 use osdiab\PHPFramework\Core\Router;
 
-require('vendor/autoload.php');
 require_once('bootstrap.php');
 Router::load($_SERVER['REQUEST_URI']);
